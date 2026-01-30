3287ea9bc190f351b2233ab15d3282ac5058a860
estevezsolutions <solucionesestevezsurl@gmail.com>
2026-01-30 17:53:54 -0500

    Actualizar main.js

--- js/main.js
+++ js/main.js
@@ -4,32 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const nav = document.querySelector('nav');
   menuToggle.addEventListener('click', () => nav.classList.toggle('show'));
 
-  // CARRUSEL
-  const slides = document.querySelectorAll('.carousel-slide');
-  const prev = document.querySelector('.prev');
-  const next = document.querySelector('.next');
-  let current = 0;
-
-  function showSlide(index) {
-    slides.forEach((slide,i) => slide.classList.toggle('active', i === index));
-  }
-
-  function nextSlide() {
-    current = (current + 1) % slides.length;
-    showSlide(current);
-  }
-
-  function prevSlide() {
-    current = (current - 1 + slides.length) % slides.length;
-    showSlide(current);
-  }
-
-  next.addEventListener('click', nextSlide);
-  prev.addEventListener('click', prevSlide);
-
-  setInterval(nextSlide, 5000); // Cambia cada 5s
-
-  // ANIMACIÓN CARDS
+  // Animación cards al hacer scroll
   const cards = document.querySelectorAll('.card');
   function showCards() {
     const triggerBottom = window.innerHeight * 0.85;
