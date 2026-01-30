2fc9566a716a07751596bbf524dcbc66d2137783
estevezsolutions <solucionesestevezsurl@gmail.com>
2026-01-30 16:57:13 -0500

    Actualizar main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let slides = document.querySelectorAll('.carousel img');
 let index = 0;
 
 setInterval(() => {
-  slides.forEach((slide)=> slide.classList.remove('active'));
+  slides.forEach(slide => slide.classList.remove('active'));
   index = (index + 1) % slides.length;
   slides[index].classList.add('active');
-},4000);
+}, 4000);
