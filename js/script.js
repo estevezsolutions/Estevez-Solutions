ba20c57dbbc00d19142fdae462917bf9341ad31a
estevezsolutions <solucionesestevezsurl@gmail.com>
2026-02-01 12:59:04 -0500

    Actualizar script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ const links = document.querySelectorAll('.menu a');
 
 window.addEventListener('scroll', () => {
     let fromTop = window.scrollY + 100;
-
     links.forEach(link => {
         let section = document.querySelector(link.getAttribute('href'));
         if (section && section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
