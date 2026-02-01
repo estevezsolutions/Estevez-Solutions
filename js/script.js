a94718577cb3936d0d24ef3cac393a764d278321
estevezsolutions <solucionesestevezsurl@gmail.com>
2026-02-01 12:11:21 -0500

    Actualizar script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-const links = document.querySelectorAll('.menu-flotante a');
+const links = document.querySelectorAll('.menu a');
 
 window.addEventListener('scroll', () => {
     let fromTop = window.scrollY + 100;
 
     links.forEach(link => {
         let section = document.querySelector(link.getAttribute('href'));
-        if (
-            section.offsetTop <= fromTop &&
-            section.offsetTop + section.offsetHeight > fromTop
-        ) {
+        if (section && section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
             links.forEach(l => l.classList.remove('activo'));
             link.classList.add('activo');
         }
